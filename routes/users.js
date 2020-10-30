df1db0cea23ef5e7f8305e65161c5a432e8f70f4
Matthew Quint <mattquint111@gmail.com>
2020-10-30 18:02:45 -0500

    created add-climb page

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
+// handles all routes at localhost:3000/
 const express = require('express')
 const router = express.Router() //express-router
 const { v4: uuidv4 } = require('uuid'); // for unique ids
 const bcrypt = require('bcrypt') //encryption
 const models = require('../models') //sequelize models //go up 2-routes to find folder
 const SALT_ROUNDS = 10 // bcrypt
+const session = require('express-session')
+
+router.get('/add-climb', (req,res) => {
+
+    res.render('users/add-climb')
+})
 
 router.get('/data', (req,res) => {
     res.render('users/data')

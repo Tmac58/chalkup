381887e3309c08e0fed6fdbfcd57247f559abfc7
Lsiopongco <58718096+luigisiops@users.noreply.github.com>
2020-11-04 15:51:18 -0500

    Map add button and popups

--- js/maps.js
+++ js/maps.js
@@ -1,21 +1,24 @@
 const apiKey = "AIzaSyCi1mGkCAUSHtMZ6pRCWg8VoVhkj5ROb1c"
-
+const testData = []
+window.addToList = function(props){
+  testData.push(props)
+  console.log(testData)
+}
 
 function init() {
-
   const successCallback = (async(position) => {
+
     const test = [
       {lat: 40.7178, lng: -74.0431},
       {lat: 40.7017, lng: -74.3222},
       {lat: 40.7128, lng: -74.0060}
     ]
-    console.log(position)
     const longitude = position.coords.longitude
     const latitude = position.coords.latitude
 
     const proxyurl = "https://cors-anywhere.herokuapp.com/";
     //const url = `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=rock+climbing&types=establishment&location=40.7017,-74.3222&radius=500&key=${apiKey}`
-    const url = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=rock+climbing&location=${latitude},${longitude}&radius=10000&fields=name,photos,formatted_address,geometry,opening_hours,rating,icon&key=${apiKey}`
+    const url = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=rock+climbing&location=${latitude},${longitude}&radius=10000&fields=name,photos,formatted_address,geometry,opening_hours,rating,opening_hours,formatted_phone_number,icon&key=${apiKey}`
     
     //get results 
 
@@ -28,11 +31,34 @@ function init() {
       zoom: 13,
     });
     console.log(data)
+
+    const infowindow = new google.maps.InfoWindow()
+    const service = new google.maps.places.PlacesService(map);
+    
+
     for (let i = 0 ; i<data.results.length; i++){
       let location = data.results[i].geometry.location
-      new google.maps.Marker({
+      const marker = new google.maps.Marker({
           position: location,
           map:map,
+      })
+      google.maps.event.addListener(marker, "click", function () {
+        infowindow.setContent(
+          `<div><strong>
+            ${data.results[i].name}
+            </strong><br>
+            Address: 
+            ${data.results[i].formatted_address} 
+            <div>
+              Rating:  
+              ${data.results[i].rating}
+              from ${data.results[i].user_ratings_total} reviews
+            </div>
+            <button onclick = "window.addToList('${data.results[i].name}')">Add</button>
+            </div>`
+        );        
+
+        infowindow.open(map, this)
       })
       
   }

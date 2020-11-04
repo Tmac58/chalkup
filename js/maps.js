80edb9eb6d3bb95b47f4a2564c13b879d2aa281d
Lsiopongco <58718096+luigisiops@users.noreply.github.com>
2020-11-04 01:56:21 -0500

    added gym finder feature

--- js/maps.js
+++ js/maps.js
@@ -1,72 +1,41 @@
+const apiKey = "AIzaSyCi1mGkCAUSHtMZ6pRCWg8VoVhkj5ROb1c"
+
 
 function init() {
-  const successCallback = (position => {
+
+  const successCallback = (async(position) => {
+    const test = [
+      {lat: 40.7178, lng: -74.0431},
+      {lat: 40.7017, lng: -74.3222},
+      {lat: 40.7128, lng: -74.0060}
+    ]
     console.log(position)
     const longitude = position.coords.longitude
     const latitude = position.coords.latitude
 
+    const proxyurl = "https://cors-anywhere.herokuapp.com/";
+    //const url = `https://maps.googleapis.com/maps/api/place/autocomplete/json?input=rock+climbing&types=establishment&location=40.7017,-74.3222&radius=500&key=${apiKey}`
+    const url = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=rock+climbing&location=${latitude},${longitude}&radius=10000&fields=name,photos,formatted_address,geometry,opening_hours,rating,icon&key=${apiKey}`
+    
+    //get results 
+
+    const result = await fetch(proxyurl + url)
+    const data = await result.json()
+
+
     const map = new google.maps.Map(document.getElementById("map"), {
       center: { lat: latitude, lng: longitude },
       zoom: 13,
-      mapTypeId: "roadmap",
     });
-    // Create the search box and link it to the UI element.
-    const input = document.getElementById("pac-input");
-    const searchBox = new google.maps.places.SearchBox(input)
-    map.controls[google.maps.ControlPosition.TOP_CENTER].push(input);
-    // Bias the SearchBox results towards current map's viewport.
-    map.addListener("bounds_changed", () => {
-      searchBox.setBounds(map.getBounds());
-    });
-    let markers = [];
-    // Listen for the event fired when the user selects a prediction and retrieve
-    // more details for that place.
-    google.maps.event.addListener(searchBox, "places_changed", () => {
-        const places = searchBox.getPlaces();
-  
-      if (places.length == 0) {
-        return;
-      }
-      // Clear out the old markers.
-      markers.forEach((marker) => {
-        marker.setMap(null);
-      });
-      markers = [];
-      // For each place, get the icon, name and location.
-      const bounds = new google.maps.LatLngBounds();
-      console.log(bounds)
-      places.forEach((place) => {
-        if (!place.geometry) {
-          console.log("Returned place contains no geometry");
-          return;
-        }
-        const icon = {
-          url: place.icon,
-          size: new google.maps.Size(71, 71),
-          origin: new google.maps.Point(0, 0),
-          anchor: new google.maps.Point(17, 34),
-          scaledSize: new google.maps.Size(25, 25),
-        };
-        // Create a marker for each place.
-        markers.push(
-          new google.maps.Marker({
-            map,
-            icon,
-            title: place.name,
-            position: place.geometry.location,
-          })
-        );
-        console.log(markers[0])
-  
-        if (place.geometry.viewport) {
-          // Only geocodes have viewport.
-          bounds.union(place.geometry.viewport);
-        } else {
-          bounds.extend(place.geometry.location);
-        }
-      });
-      map.fitBounds(bounds);
-    });
+    console.log(data)
+    for (let i = 0 ; i<data.results.length; i++){
+      let location = data.results[i].geometry.location
+      new google.maps.Marker({
+          position: location,
+          map:map,
+      })
+      
+  }
   })
   const errorCallback = (error) => {
     console.log(error)
